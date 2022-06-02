b538d7a3339f1bd2831a554e030889fe818dab6d
dmitrii_lakhmenev <lahmenev@gmail.com>
2022-06-02 20:16:08 +0300

    refactoring code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-from utils import get_log_list, set_filter, set_map
+from utils import get_log_list, query_builder
+
 app = Flask(__name__)
 
 
@@ -16,40 +17,10 @@ def perform_query():
     if file_name is not None:
         list_log = get_log_list(file_name)
         if list_log:
-            #  Фильтрация данных
-            if cmd1 == 'filter' and len(value1) > 0:
-                list_log = set_filter(list_log, value1)
-            elif cmd2 == 'filter' and len(value2) > 0:
-                list_log = set_filter(list_log, value2)
+            list_log = query_builder(list_log, cmd1, value1)
+            list_log = query_builder(list_log, cmd2, value2)
 
-            #  Изменение формата вывода
-            if cmd1 == 'map' and value1.isdigit():
-                list_log = set_map(list_log, int(value1))
-            elif cmd2 == 'map' and value2.isdigit():
-                list_log = set_map(list_log, int(value2))
-
-            #  Уникальные значения
-            if cmd1 == 'unique' or cmd2 == 'unique':
-                list_log = list(set(list_log))
-
-            # Сортировка данных
-            if cmd1 == 'sort':
-                if value1 == 'desc':
-                    list_log = sorted(list_log, reverse=True)
-                else:
-                    list_log = sorted(list_log, reverse=False)
-            elif cmd2 == 'sort':
-                if value2 == 'desc':
-                    list_log = sorted(list_log, reverse=True)
-                else:
-                    list_log = sorted(list_log, reverse=False)
-
-            #  Лимит вывода
-            if cmd1 == 'limit' and value1.isdigit():
-                list_log = list_log[:int(value1)]
-            elif cmd2 == 'limit' and value2.isdigit():
-                list_log = list_log[:int(value2)]
-
+            #  Добавим перенос строки для вывода
             list_log = [item + '\n' for item in list_log]
 
             return app.response_class(list_log, 200, content_type="text/plain")
--- utils.py
+++ utils.py
@@ -7,8 +7,10 @@ def get_log_list(log_name: str):
     if Path.is_file(file_path):
         with open(file_path, 'r', encoding='utf8') as txt_file:
             for line in txt_file:
-                line.replace('\n', '')
                 log_list.append(line)
+
+    #  Удаляем переносы строк и конечные пробелы
+    log_list = [line.rstrip() for line in log_list]
     return log_list
 
 
@@ -18,9 +20,32 @@ def get_format(in_string: str, col: int):
     return out_string
 
 
-def set_filter(in_list: list, filter_str: str):
+def set_filter(in_list: list, filter_str: str) -> list:
     return list(filter(lambda x: filter_str in x, in_list))
 
 
 def set_map(in_list: list, column: int):
     return list(map(lambda x: get_format(x, column), in_list))
+
+
+def query_builder(iterable_var: list, cmd, value) -> list:
+    if cmd == 'unique':
+        return list(set(iterable_var))
+
+    if value:
+        if cmd == 'filter':
+            iterable_var = set_filter(iterable_var, value)
+
+        elif cmd == 'map':
+            iterable_var = list(map(lambda x: get_format(x, int(value)), iterable_var))
+
+        elif cmd == 'limit':
+            iterable_var = iterable_var[:int(value)]
+
+        elif cmd == 'sort':
+            if value == 'desc':
+                iterable_var = sorted(iterable_var, reverse=True)
+            else:
+                iterable_var = sorted(iterable_var, reverse=False)
+
+    return iterable_var
